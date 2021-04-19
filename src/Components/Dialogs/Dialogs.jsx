0f19d0fd6cc68b6d5a3351637a1e132aba9d6339
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2021-04-19 17:38:46 +0300

    Add files via upload

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -5,46 +5,60 @@ import {NavLink} from "react-router-dom";
 const DialogItem = (props) => {
     let path = '/dialogs/' + props.id;
     return (
-        <div className={s.dialog + " " + s.active}>
-            <NavLink to={path}>{props.name}</NavLink> {/*'/dialogs/'+ props.id* ИЛИ ТАК СДЕлАТЬ вместо PATH!*/}
+        <div className={`${s.dialog} ${s.active}`}>
+            <NavLink to={path} activeClassName={s.active}>{props.name}</NavLink> {/*'/dialogs/'+ props.id* ИЛИ ТАК СДЕлАТЬ вместо PATH!*/}
         </div>
-    )
+    );
 }
+
 const Message = (props) => {
 
     return <div className={s.dialog}>{props.message}</div>
 
-
 }
 const Dialogs = (props) => {
-    let dialogsData = [{id: 1, name: 'Dima'},
-        {id: 2, name: 'Sveta'},
-        {id: 3, name: 'Vladimir'},
-        {id: 4, name: 'Stas'},
-        {id: 5, name: 'Genadiy'},
-        {id: 6, name: 'Vlad'},
-        {id: 7, name: 'Tanya'}];
+    let newDialogsElement=React.createRef();
+    let addDialogs=()=>{
+        let dialogs=newDialogsElement.current.value;
+        alert(dialogs);
+    }
+let newMessageElement=React.createRef();
+let addMessage=()=>{
+let message=newMessageElement.current.value;
+alert(message);
+}
 
-    let messagesData = [{id: 1, message: 'How are you?'},
-        {id: 2, message: 'Good'},
-        {id: 3, message: 'Ok'},
-        {id: 4, message: 'Ok'},
-        {id: 5, message: 'Ok'}];
 
-    let DialogsElement = dialogsData
-        .map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = messagesData
-        .map(m => <Message message={m.message}/>)
+
+    let DialogsElement = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = props.state.messagesData.map(m => <Message message={m.message}/>)
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                {
-                    DialogsElement
-                }
+
+                {DialogsElement}
+                <div className={s.textarea} >
+                    <textarea ref={newDialogsElement}> </textarea>
+                </div>
+                <div className={s.button}>
+                    <button onClick={addDialogs}>
+                        отправить
+                    </button>
+                </div>
+
             </div>
             <div className={s.messages}>
-                {messagesElements}
+                { messagesElements }
+                <div className={s.textarea}>
+                    <textarea ref={newMessageElement}> </textarea>
+                </div>
+                <div className={s.button}>
+                    <button onClick={addMessage}>
+                        отправить
+                    </button>
+                </div>
             </div>
+
         </div>
 
 
