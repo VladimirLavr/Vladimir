0a3872fd46f0e2f7541fd9738f75f59255acbf9a
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2021-04-19 17:38:06 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./Redux/State";
+import {BrowserRouter} from "react-router-dom";
+import {addPost} from "./Redux/State";
+import {rerenderEntireTree} from "./render";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+rerenderEntireTree(state);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
