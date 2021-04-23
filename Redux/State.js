f90d03621f96cb8cdbf8643f294eff8be55cb237
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2021-04-23 16:44:02 +0300

    Add files via upload

--- Redux/State.js
+++ Redux/State.js
@@ -1,48 +1,63 @@
-let rerenderEntireTree=()=>{
-    console.log('state changed');
-}
+let store= {
+    _state: {
+        messagePage: {
+            posts: [{id: 0, message: 'Hi,how are you?', likesCount: 12},
+                {id: 1, message: 'It\'s my first post', likesCount: 11}],
+            newPostText: 'Lavrikov!!'
 
-let state= {
-    messagePage: {
-        posts: [{id: 0, message: 'Hi,how are you?', likesCount: 12},
-            {id: 1, message: 'It\'s my first post', likesCount: 11}],
-        newPostText:'Lavrikov!!'
+        },
+        DialogsPage: {
+            messagesData: [{id: 1, message: 'How are you?'},
+                {id: 2, message: 'Good'},
+                {id: 3, message: 'Ok'},
+                {id: 4, message: 'Ok'},
+                {id: 5, message: 'Ok'}],
+            dialogsData: [{id: 1, name: 'Dima'},
+                {id: 2, name: 'Sveta'},
+                {id: 3, name: 'Vladimir'},
+                {id: 4, name: 'Stas'},
+                {id: 5, name: 'Genadiy'},
+                {id: 6, name: 'Vlad'},
+                {id: 7, name: 'Tanya'}]
+        }
+    },
+    getState(){
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('state changed');
 
     },
-    DialogsPage: {
-        messagesData: [{id: 1, message: 'How are you?'},
-            {id: 2, message: 'Good'},
-            {id: 3, message: 'Ok'},
-            {id: 4, message: 'Ok'},
-            {id: 5, message: 'Ok'}],
-        dialogsData: [{id: 1, name: 'Dima'},
-            {id: 2, name: 'Sveta'},
-            {id: 3, name: 'Vladimir'},
-            {id: 4, name: 'Stas'},
-            {id: 5, name: 'Genadiy'},
-            {id: 6, name: 'Vlad'},
-            {id: 7, name: 'Tanya'}]
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.messagePage.newPostText,
+            likesCount: 0
+        };
+        this._state.messagePage.posts.push(newPost);
+        this._state.messagePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+
+        this._state.messagePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
 
-export const addPost=( )=>{
-    let newPost={
-        id:5,
-        message: state.messagePage.newPostText ,
-        likesCount:0
-    };
-    state.messagePage.posts.push(newPost);
-    state.messagePage.newPostText='';
-    rerenderEntireTree(state);
-}
 
-export const updateNewPostText=(newText)=>{
 
-    state.messagePage.newPostText=newText;
-    rerenderEntireTree(state);
-}
-export const subscribe=(observer)=>{
-    rerenderEntireTree=observer;
-}
-export default state;
+
+
+
+
+
+
+
+
+export default store;
+    window.store=store;
